a86b6316744083b7f754875ee1f3bd4fd8c36692
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-08-31 19:55:26 -0300

    Finanlizando o componente de Filtros

--- dashboard/src/assets/componentes/Filtros.js
+++ dashboard/src/assets/componentes/Filtros.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import filtro from '../icones/filtro.svg';
 
 export const Filtros = () => {
+  const [showFiltros, setShowFiltros] = useState(false);
+
   return (
     <div className="container-filtros">
       <div className="title">
         <span>Tarefas</span>
-        <img src={filtro} alt="Filtrar tarefas" />
+        <img
+          src={filtro}
+          alt="Filtrar tarefas"
+          onClick={() => setShowFiltros(!showFiltros)}
+        />
         <div className="form">
           <div>
             <label>Data prevista de conclusão de:</label>
@@ -28,6 +34,26 @@ export const Filtros = () => {
           </div>
         </div>
       </div>
+      {showFiltros === true && (
+        <div className="filtrosMobile">
+          <div>
+            <label>Período de:</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Período até:</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Status:</label>
+            <select>
+              <option value={0}>Todas</option>
+              <option value={1}>Ativas</option>
+              <option value={2}>Concluídas</option>
+            </select>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
