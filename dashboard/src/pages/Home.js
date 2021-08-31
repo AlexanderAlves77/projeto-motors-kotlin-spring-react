9fd3821eca98f85e11da97b905806c330bf55164
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-08-31 20:42:05 -0300

    Iniciando a listagem de tarefas

--- dashboard/src/pages/Home.js
+++ dashboard/src/pages/Home.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../assets/componentes/Header';
 import { Filtros } from '../assets/componentes/Filtros';
 import { Listagem } from '../assets/componentes/Listagem';
 import { Footer } from '../assets/componentes/Footer';
 
 export const Home = props => {
+  const [tarefas, setTarefas] = useState([
+    {
+      id: '123wamaks',
+      nome: 'Tarefa Mock 1',
+      dataPrevisaoConclusao: '2021-08-31',
+      dataConclusao: null,
+    },
+    {
+      id: 'wamaks123',
+      nome: 'Tarefa Mock 2',
+      dataPrevisaoConclusao: '2021-08-31',
+      dataConclusao: '2021-09-01',
+    },
+  ]);
+
   const sair = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('usuarioNome');
@@ -16,7 +31,7 @@ export const Home = props => {
     <>
       <Header sair={sair} />
       <Filtros />
-      <Listagem />
+      <Listagem tarefas={tarefas} />
       <Footer />
     </>
   );
